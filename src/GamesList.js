d971be07bf40dcf54126a2c39a73232dd5bdc09a
Benatovich <sambenatovich@gmail.com>
2022-02-10 20:17:44 -0500

    commented out like 30 lines of redundant code in GamesList.js

--- src/GamesList.js
+++ src/GamesList.js
@@ -2,39 +2,39 @@ import React, { Component, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getGames } from './services/fetch-utils';
 
-// functional component
-export function __GamesList({ games }) {
+// // functional component
+// export function __GamesList({ games }) {
 
-    // this.setState({ count: count + 1 })
-  const [count, setCounter] = useState(0);
+//     // this.setState({ count: count + 1 })
+//   const [count, setCounter] = useState(0);
 
-//  function called componentDidMount
-  useEffect(() => {
-    console.log('this happens on load');
-  }, []);
+// //  function called componentDidMount
+//   useEffect(() => {
+//     console.log('this happens on load');
+//   }, []);
 
-  // this happens in a function called componentDidUpdate
-  useEffect(() => {
-    console.log('nice job updating that counter!');
-  }, [count]);
+//   // this happens in a function called componentDidUpdate
+//   useEffect(() => {
+//     console.log('nice job updating that counter!');
+//   }, [count]);
 
-  // the JSX is rendered in a function called "render" (duh)
-  return <div>
-    <button onClick={() => setCounter(count + 1)}>Add to count</button>
-    {/* in CBC this.state.count */}
-    <h2>{count}</h2>
-    {
-      games.map(game => <Link key={game.id + game.title} to={`/board-games/${game.id}`}>
-        <div className='game'>
-          <h3>{game.title}</h3>
-          <p>A {game.genre} game</p> 
-          <p>Designed by {game.designer}</p>
-          <p>For {game.min_players} - {game.max_players} players</p>
-        </div>
-      </Link>)
-    }
-  </div>;
-}
+//   // the JSX is rendered in a function called "render" (duh)
+//   return <div>
+//     <button onClick={() => setCounter(count + 1)}>Add to count</button>
+//     {/* in CBC this.state.count */}
+//     <h2>{count}</h2>
+//     {
+//       games.map(game => <Link key={game.id + game.title} to={`/board-games/${game.id}`}>
+//         <div className='game'>
+//           <h3>{game.title}</h3>
+//           <p>A {game.genre} game</p> 
+//           <p>Designed by {game.designer}</p>
+//           <p>For {game.min_players} - {game.max_players} players</p>
+//         </div>
+//       </Link>)
+//     }
+//   </div>;
+// }
 
 export default class GamesList extends Component {
   constructor() {
